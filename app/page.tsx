9d26ae16259b96abc97dbce45e5800f074d0fc11
mausooq <mauchoomausooq051@gmail.com>
2025-11-07 15:50:27 +0530

    fixed ui

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       </header>
       <main className="flex-1 flex items-center justify-center text-center px-6">
         <div className="max-w-xl">
-          <Image src="/logo-02.svg" alt="Urban Biz Tech" width={240} height={144} priority className="mx-auto mb-4 h-auto w-[240px]" />
+          <Image src="/logo-white.svg" alt="Urban Biz Tech" width={240} height={144} priority className="mx-auto mb-4 h-auto w-[240px]" />
     
           <p className="text-lg md:text-xl text-gray-300 mb-8">
             Innovating the future of urban businesses.
